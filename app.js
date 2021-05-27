92d78d7dec98c87cb685b9673ade49f208306c79
Mechirv <mechivivanco@hotmail.com>
2021-05-27 20:01:23 -0300

    27/05

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 app.use(express.static(path.resolve(__dirname,'./public')));
-app.listen(3000, () => console.log("sever start in http://localhost:3000"));
+app.set("port",process.env.PORT || 3000)
+app.listen(app.get("port"), () => console.log("sever start in http://localhost:3000"));
 
 app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","home.html")));
 app.get("/register", (req,res)=>res.sendFile(path.join(__dirname,"/views","register.html")));
